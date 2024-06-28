1c359337142838d4a8947537f5e39efce63d86a1
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2024-06-28 10:03:14 +0300

    make it pass the tests actually

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from db import models
 from db.database_definition import engine
 
 app = FastAPI()
+
+@app.get("/")
+def index():
+    return RedirectResponse(url="/docs")
 
 
 models.Base.metadata.create_all(bind=engine)
